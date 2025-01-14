50226894604f0adf7138580accebdf280b8692e7
nusratalam123 <nusratalam975@gmail.com>
2025-01-14 12:33:25 +0600

    add all route

--- src/shared/route.ts
+++ src/shared/route.ts
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import authRoutes from "./../routes/auth.route";
-
 import userRoutes from "./../routes/user.route";
-import carRoutes from "./../routes/carPost.route";
-import bookingRoutes from "./../routes/book.route";
+import healthDataRoutes from "../routes/healthData.route";
+import bookingRoutes from "../routes/notification.route";
+import medicationRoutes from "../routes/medication.route";
+import doctorRoutes from "../routes/doctor.route";
+import appointmentRoutes from "../routes/appointment.route";
+import testimonialRoutes from "../routes/testimonial.route";
+import eventRoutes from "../routes/event.route";
+import foodRoutes from "../routes/food.route";
+import healthSuggestionRoutes from "../routes/healthSuggestionData.route";
+
+
+
+
+
+
+
 const router = Router();
 
 // Root route
@@ -14,8 +27,20 @@ router.get("/", (_, res) => {
 // general Routes
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
-router.use("/car", carRoutes);
+router.use("/health-data", healthDataRoutes);
 router.use("/book", bookingRoutes);
+router.use("/medications", medicationRoutes);
+router.use("/doctor", doctorRoutes);
+router.use("/appointment", appointmentRoutes);
+router.use("/testimonial", testimonialRoutes);
+router.use("/event", eventRoutes);
+router.use("/food", foodRoutes);
+router.use("/health-suggestion", healthSuggestionRoutes);
+
+
+
+
+
 // Handle not found
 router.use((req, res, next) => {
   res.status(404).json({
